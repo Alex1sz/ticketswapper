0db88e8cb949217080ccc3e8aad073cb78fcef1e
Alex1sz <szabo@email.arizona.edu>
2015-03-23 21:21:07 -0700

    Adds solr search feature for searching tickets

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def search
+    if params[:query].blank?
+      flash[:notice] = "Please supply a search term"
+    else
+      @ticket_results = Ticket.search_ticket(params[:query])
+      if @ticket_results.nil?
+        flash[:notice] = "No results found"
+      else
+        @search = @ticket_results
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
+
   resources :tickets
+
+  get :search, :to => 'static_pages#search', :as => 'search'
+
   get 'users/dashboard', to: 'users#dashboard'
   resources :payments, only: [:new, :create]
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
