4124c6581af6b15e2d717bc7a4c839d6120073e0
Alex1sz <szabo@email.arizona.edu>
2015-04-14 16:09:25 -0700

    Moves mixpanel script out of layout into its own js file. Adds full_title method into application helper

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require mixpanel
 //= require semantic
 $(document).semantic();
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def format_date(date)
     date.strftime('%B %e, %Y')
   end
+
+  def full_title(page_title = '')
+    base_title = "SwapStubs"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
