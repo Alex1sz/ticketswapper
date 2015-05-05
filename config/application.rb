3d3805dc5711a4e92c1c20f14f3274e7560078d5
Alex1sz <szabo@email.arizona.edu>
2015-05-05 16:35:34 -0700

    Adds icons settings to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,11 @@ module Ticketswapper
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.serve_static_files = true
+
+    # add app/assets/fonts to asset path
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile += %w( .svg .eot .woff .tff)
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
